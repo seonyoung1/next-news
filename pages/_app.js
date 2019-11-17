ffa55faef60f39f083ad5af6f95478f77028e617
김선영 <tjsgur35@betree.co.kr>
2019-11-17 19:01:40 +0900

    antd css적용

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from "next/head";
 import Layout from "../components/Layout";
 import PropTypes from "prop-types";
+import 'antd/dist/antd.css';
 import "../styles/common.scss";
 
 const News = ({Component}) => {
--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useInput} from "../hooks";
+import {DatePicker} from "antd";
 
 const Search = () => {
     const [keyword, onchange] = useInput();
@@ -8,10 +9,17 @@ const Search = () => {
 
     };
 
+    const range = (date, dateString) => {
+        console.log(date, dateString);
+    };
+
     return (
         <form onSubmit={onSubmit}>
             <input type="text" value={keyword} onChange={onchange} />
             <button type="submit">검색</button>
+
+            <p>상세검색</p>
+            <DatePicker.RangePicker onChange={range} />
         </form>
     );
 };
