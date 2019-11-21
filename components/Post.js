6e2d93c9ae0a00b901878fee6479488adba66af0
김선영 <tjsgur35@betree.co.kr>
2019-11-21 20:10:38 +0900

    eslint prettier setting

--- components/Post.js
+++ components/Post.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 
-const Post = ({data}) => {
-    if(!data) return;
-    return (
-        <>
-            <div className="thumb">
-                <div><div><img src={(data.urlToImage&&data.urlToImage.includes("http"))?data.urlToImage:"/images/default.jpg"} alt={data.title} /></div></div>
-            </div>
-            <div className="contents">
-                <div className="title">{data.title}</div>
-                <div className="desc">{data.description}</div>
-                <a href={data.url} target="_blank">더보기</a>
-            </div>
-        </>
-    );
+const Post = ({ data }) => {
+	if (!data) return;
+	return (
+		<>
+			<div className="thumb">
+				<div>
+					<div>
+						<img src={data.urlToImage && data.urlToImage.includes("http") ? data.urlToImage : "/images/default.jpg"} alt={data.title} />
+					</div>
+				</div>
+			</div>
+			<div className="contents">
+				<div className="title">{data.title}</div>
+				<div className="desc">{data.description}</div>
+				<a href={data.url} rel="noopener noreferrer" target="_blank">
+					더보기
+				</a>
+			</div>
+		</>
+	);
 };
 
 Post.propTypes = {
-    data: PropTypes.object,
-    title: PropTypes.string,
+	data: PropTypes.object,
+	title: PropTypes.string,
 };
 
 export default Post;
